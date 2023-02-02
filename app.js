96463eea219e7971e3b12ad410ac1cc57554da3b
SAMBIT MAJHI <majhisambit2@gmail.com>
2023-02-02 21:57:27 +0530

    up

--- app.js
+++ app.js
@@ -3,13 +3,41 @@ const app = express();
 const cors = require("cors");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const { PrismaClient } = require('@prisma/client')
-
+// const { PrismaClient } = require('@prisma/client')
+const d = require('@directus/sdk');
+const directus = new d.Directus('https://admin.ezchat.agpro.co.in');
 app.use(express.json());
 app.use(cors());
+let authenticated = false;
+async function auth() {
+  // AUTHENTICATION
 
-const prisma = new PrismaClient()
+  authenticated = false;
 
+  // Try to authenticate with token if exists
+  await directus.auth
+    .refresh()
+    .then(() => {
+      authenticated = true;
+    })
+    .catch(() => { });
+
+  // Let's login in case we don't have token or it is invalid / expired
+  while (!authenticated) {
+    const email = "messagesaver@ezchat.com";
+    const password = "12345678";
+
+    await directus.auth
+      .login({ email, password })
+      .then(() => {
+        authenticated = true;
+        console.log('Authenticated');
+      })
+      .catch(() => {
+        console.log('Invalid credentials');
+      });
+  }
+}
 // socket
 
 var users = {};
@@ -18,9 +46,23 @@ io.on("connection", (socket) => {
   console.log(`user ${socket.handshake.query.user} connected`);
   users[socket.handshake.query.user] = socket.id;
   console.log(users);
-  socket.on("message", (msg) => {
-    console.log(msg);
+  socket.on("message", async (msg) => {
+    await auth();
     io.to(users[msg.receiver]).emit("passmessage", msg);
+    if (authenticated) {
+     await directus.items('message').createOne({
+        text: msg.text,
+        sender: msg.sender,
+        receiver: msg.receiver,
+        time: msg.time,
+        messageType: msg.messageType,
+        messageStatus: 'sent',
+      }).then(data => {
+        console.log(data);
+      }).catch(error => {
+        console.log(error);
+      });
+    }
   });
   socket.on("disconnect", (socket) => {
     console.log(`user disconnected`);
@@ -28,8 +70,8 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", async (req, res) => {
-  const allMessages = await prisma.messages.findMany();
-  console.dir(allMessages, { depth: null })
+  // const allMessages = await prisma.messages.findMany();
+  // console.dir(allMessages, { depth: null })
   res.send("Hello World! <a href='/chat'>Chat</a>");
 
 });
